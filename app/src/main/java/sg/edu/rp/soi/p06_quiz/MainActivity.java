8997df690978f78f664353f44f1f618fd74d2a81
18039253 <18039253@myrp.edu.sg>
2019-05-24 15:56:42 +0800

    ren yujia

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -2,16 +2,66 @@ package sg.edu.rp.soi.p06_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNum1, etNum2;
+    TextView tvOperation, tvResult;
+    Button btnReset;
 
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
+        tvOperation = findViewById(R.id.tvOperation);
+        tvResult = findViewById(R.id.tvResult);
+        btnReset = findViewById(R.id.btnReset);
+
+        registerForContextMenu(tvOperation);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+            }
+        });
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
+        menu.add(0,0,0,"+");
+        menu.add(0,1,1,"-");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 0){
+            int num1 = Integer.parseInt(etNum1.getText().toString());
+            int num2 = Integer.parseInt(etNum2.getText().toString());
+            int sum = num1 + num2;
+            tvResult.setText(sum+"");
+            return true;
+        }
+        else if(item.getItemId() == 1){
+            int num1 = Integer.parseInt(etNum1.getText().toString());
+            int num2 = Integer.parseInt(etNum2.getText().toString());
+            int sub = num1-num2;
+            tvResult.setText(sub+"");
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 }
